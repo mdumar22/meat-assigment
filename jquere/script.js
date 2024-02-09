1dafe1c12115b7e965bc34f1b249b1558810dca3
mdumar22 <mdumarparweez@gmail.com>
2024-02-09 01:47:15 -0800

    vffvedsvefds
    cffdsvcesfd

--- jquere/script.js
+++ jquere/script.js
@@ -1,2 +1,13 @@
+jQuery(document).ready(function($){
+
+$('.side-bar').click(function(){
+  $('.nav_wrapper').addClass('open_menu')
+})
+$('.cross_toggel').click(function(){
+  $('.nav_wrapper').removeClass('open_menu')
+})
+
 
   AOS.init();
+
+})
